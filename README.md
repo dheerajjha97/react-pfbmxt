0182ec8362972841b13d3af8073dec2c274ed754
dheerajjha97 <jhadheeraj97@gmail.com>
2021-10-16 15:45:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-zpniw6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-zpniw6)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App=()=>{
+  return(
+  
+  <h1>
+Hell
+    </h1>
+    
+    )
+}
+
+export default App;
